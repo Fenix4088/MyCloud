701a123cc71c9c95271eae9d7a18f75dd8f3467c
Yehor Pliasov <fenix4088@gmail.com>
2021-09-01 22:58:02 +0300

    Figma link added to store.js

--- front/my-cloud/src/redux/store.js
+++ front/my-cloud/src/redux/store.js
@@ -4,6 +4,8 @@ import thunk from "redux-thunk";
 import {userReducer} from "./reducers/userReducer/userReducer";
 import {fileReducer} from "./reducers/fileReducer/fileReducer";
 
+// ! Figma https://www.figma.com/file/TDTKrjz93RzQh3nuzpws8H/MERN-CLOUD-DISK?node-id=2%3A100
+
 const rootReducer = combineReducers({
     userReducer,
     fileReducer
