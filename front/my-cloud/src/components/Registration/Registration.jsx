edb14fd8f8463611e4f50821e80f66644b89d262
Yehor Pliasov <fenix4088@gmail.com>
2021-09-03 09:45:20 +0300

    feature: front Registration form finished

--- front/my-cloud/src/components/Registration/Registration.jsx
+++ front/my-cloud/src/components/Registration/Registration.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import s from './Registration.module.scss';
-
+import { Input } from '../Input/Input';
 
 export const Registration = () => {
-    return (
-        <div className={s['wrapper']}>
-            <form className={s['form']}>
-                <h2>Registration</h2>
-                <input type="text" placeholder={'Enter your name...'}/>
-                <input type="text" placeholder={'Enter your surname...'}/>
-                <input type="email" placeholder={'Enter your email...'}/>
-                <input type="password" placeholder={'Enter your password...'}/>
-                <button type={'submit'}>Submit</button>
-            </form>
-        </div>
-    )
-}
+  const [{ email, password }, setInputValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const onInputChange = useCallback((e) => {
+    const { name, value } = e.currentTarget;
+    setInputValues((state) => ({ ...state, [name]: value }));
+  }, [setInputValues]);
+
+  return (
+    <div className={s['wrapper']}>
+      <form className={s['form']}>
+        <h2>Registration</h2>
+        <Input value={email} type='email' name={'email'} placeholder={'Enter your email...'} onChange={onInputChange} />
+        <Input value={password} type='password' name={'password'} placeholder={'Enter your password...'} onChange={onInputChange} />
+        <button type={'submit'}>Submit</button>
+      </form>
+    </div>
+  );
+};
